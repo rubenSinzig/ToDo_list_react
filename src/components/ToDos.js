c150d675c9e58d6f48645519a617ec8483aba9e6
rubenSinzig <ruben.sinzig@posteo.de>
2021-05-31 15:10:32 +0200

    ToDo -> scss

--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -2,6 +2,7 @@ const ToDos = (props) => {
   const userTodos = props.list.map((toDo, i) => (
     <li key={i}>
       <input
+        className="checkbox"
         type="checkbox"
         defaultChecked={toDo.done}
         onChange={() => props.check(toDo.id)}
